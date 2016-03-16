0dee4fb90a93d12f36440af8f1b14b20bf8d0910
aoki <aoki@cubit.jp>
2016-03-16 13:54:03 +0900

    item Window select

--- src/js/Window.ts
+++ src/js/Window.ts
@@ -14,17 +14,31 @@ export class Window {
 
 
     constructor(game,array) {
+        let defaultNum = 0;
+        let _this = this;
         switch (array['type']){
             case 'item':
-                this.itemWindow(game,array)
+                _this.itemWindow(game,array,defaultNum);
                 break;
             case 'other':
                 console.log('それ以外');
                 break;
         }
+
+        game.addEventListener( 'enterframe', function(){
+            if(game.input.down) {
+                defaultNum ++;
+                _this.itemWindow(game,array,defaultNum);
+            }
+            if(game.input.up) {
+                defaultNum --;
+                _this.itemWindow(game,array,defaultNum);
+            }
+        });
+
     }
 
-    itemWindow(game,itemListArray) {
+    itemWindow(game,itemListArray,defaultNum) {
         let itemWindowWidth = Window.ITEM_WIDTH + Window.WINDOW_PADDING * 2;
         let itemWindowHeight = Window.ITEM_HEIGHT * itemListArray['data'].length + Window.WINDOW_PADDING_HEIGHT * 2;
         let windowBg = new Sprite(itemWindowWidth,itemWindowHeight);
@@ -37,11 +51,15 @@ export class Window {
         windowBgS.context.fill();
         windowBgS.context.stroke();
         windowBg.image = windowBgS;
+
+        game.rootScene.removeChild(windowBg);
         game.rootScene.addChild(windowBg);
 
         for (var i = 0; i < itemListArray['data'].length; i++) {
             let itemList = new Sprite(Window.ITEM_WIDTH,Window.ITEM_HEIGHT);
-            //itemList.backgroundColor = '#000';
+            if (i == defaultNum ) {
+                itemList.backgroundColor = 'rgba(255, 255, 255, .2)';
+            }
             itemList.x = windowBg.x + Window.WINDOW_PADDING;
             itemList.y = Window.ITEM_HEIGHT * i + windowBg.y + Window.WINDOW_PADDING_HEIGHT;
     		itemList.opacity = 0.7;
@@ -54,6 +72,8 @@ export class Window {
     		label.x = itemList.x + Settings.CHIP_WIDTH / 3;
     		label.y = itemList.y + 1.5;
 
+            game.rootScene.removeChild(itemList);
+            game.rootScene.removeChild(label);
             game.rootScene.addChild(itemList);
     		game.rootScene.addChild(label);
         }
